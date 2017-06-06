45abfaf325368b6636d8af9309008dc23e019fec
EC2 Default User <ec2-user@ip-172-31-14-152.us-west-2.compute.internal>
2017-06-06 20:34:19 +0000

    Use Hibernate 3 not 4, fix h2 relative path not allowed

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -5,12 +5,21 @@ dataSource {
     username = "sa"
     password = ""
 }
+/*
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
 //    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
     cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
+}
+*/
+hibernate {
+  cache.use_second_level_cache = true
+  cache.use_query_cache = false
+  cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+//cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+  singleSession = true // configure OSIV singleSession mode
 }
 
 // environment specific settings
@@ -30,7 +39,7 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:h2:~/prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE" //craig added ~/
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
